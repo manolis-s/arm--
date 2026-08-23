a69e20490a967a529055b2b4f05c5a94d1175651
Manolis <siganos.manolis@gmail.com>
2026-08-23 13:41:06 +0300

    μεταφορά σε cloud περιβάλλον

--- database.py
+++ database.py
@@ -1,11 +1,17 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./armex.db"
+# Ζητάμε το URL της βάσης από το Render. 
+# Αν δεν το βρει (π.χ. στον υπολογιστή σου), φτιάχνει προσωρινά το παλιό sqlite.
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./armex.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Η PostgreSQL δεν θέλει το "check_same_thread" που ήθελε το SQLite
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
